c1a76039dac268bad5b790e22e189b488e88a734
Denis Kozlov <ConstrayRUS@gmail.com>
2017-05-13 21:32:37 +0400

    fft doing weird stuff

--- ffttest.py
+++ ffttest.py
@@ -1,18 +1,47 @@
 import matplotlib.pyplot as plt
-import scipy
-from scipy.fftpack import fftshift
+from scipy.fftpack import fftshift, fft, fftfreq
+import copy
 import numpy as np
+from sympy import Heaviside, sin, cos, symbols
 
+t = symbols("t", positive=True)
+N=200
+Tau = 20
+A = 10
+signal = A*(1 - 2*Heaviside(t-Tau/2) + Heaviside(t-Tau))
 
+# Задаём шаг
+T = Tau/N
+x = np.linspace(0.0, N * T, N)
+y = []
+f2 = copy.deepcopy(signal)
+# Заполняем массив значений сигнала
+for i in x:
+    if int(i) == i:
+        f2 = f2.subs(Heaviside(t - int(i)), 1)
+    else:
+        f2 = f2.subs(Heaviside(t - i), 1)
+    y.append(f2.subs(t, i))
+# Заполняем значения амплитудного спектра
+yf = fftshift(fft(y))
+# Заполняем частоты
+xf = np.linspace(-1.0 / (2.0 * T), 1.0 / (2.0 * T), N)
+yf2 = copy.deepcopy(2.0 / N * yf)
+# Фильтруем помехи
+threshold = np.max(np.abs(yf2)) / 10000
+for i in range(len(yf2)):
+    if abs(yf2[i]) < threshold:
+        yf2[i] = 0
+xp = []
+yp = []
+# Заполняем новые массивы без помех
+for i in range(len(yf2)):
+    if yf2[i] != 0:
+        xp.append(xf[i])
+        yp.append(yf2[i])
+yp = np.angle(yp)
+yf = np.abs(yf)
 
-x = np.arange(0, 3, 0.01)
-y = np.zeros(len(x))
-y[0:150] = 1
-#plt.plot(x, y) # plot of the step function
-
-yShift = fftshift(y) # shift of the step function
-Fourier = scipy.fft(yShift) # Fourier transform of y implementing the FFT
-Fourier = fftshift(Fourier) # inverse shift of the Fourier Transform
-plt.plot(Fourier) # plot of the Fourier transform
-#plt.plot(np.angle(Fourier))
+plt.plot(xf,yf)
+plt.grid()
 plt.show()
--- laplacetest.py
+++ laplacetest.py
@@ -1,14 +1,46 @@
-import sympy as sp
-from sympy.abc import t, s
 import numpy as np
-import matplotlib.pyplot as plt
+from sympy.abc import symbols
+from sympy import I, im, re
+
+def talbot_inverse(f_s, t_z, M = 64):
+    k = np.arange(M)
+    delta = np.zeros(M, dtype=complex)
+    for i in k:
+        if i != 0:
+            delta[i] = 2*np.pi/5 * i * (np.tan(np.pi/M*i)**(-1)+1.j)
+    delta[0] = 2*M/5
+    gamma = np.zeros(M, dtype=complex)
+    for i in k:
+        if i != 0:
+            gamma[i] = (1 + 1.j*np.pi/M*i*(1+np.tan(np.pi/M*i)**(-2))-1.j*np.tan(np.pi/M*i)**(-1))*np.exp(delta[i])
+    gamma[0] = 0.5*np.exp(delta[0])
+    delta_mesh, t_mesh = np.meshgrid(delta, t_z)
+    gamma_mesh = np.meshgrid(gamma,t_z)[0]
+    points = delta_mesh/t_mesh
+    fun_res_ch = np.zeros(np.shape(points), dtype=complex)
+    fun_res_zn = np.zeros(np.shape(points), dtype=complex)
+    for i in range(len(f_s[0])):
+        fun_res_ch = fun_res_ch + f_s[0][i]*points**(len(f_s[0]) - i)
+    for i in range(len(f_s[1])):
+        fun_res_zn = fun_res_zn + f_s[1][i]*points**(len(f_s[1]) - i)
+    fun_res = fun_res_ch/fun_res_zn
+    sum_ar = np.real(gamma_mesh*fun_res)
+    sum_ar = np.sum(sum_ar, axis = 1)
+    ilt = 0.4/t_z * sum_ar
+    return ilt
 
 
-f = sp.laplace_transform(5*sp.sin(t) + 2*t ,t,s)
-x = np.arange(0, 3, 0.1)
-y = []
-for i in x:
-    y.append(f[0].subs(s, i))
-plt.plot(x,y)
-plt.show()
+
+# h = (s**2+0.25*s+12)/(s**2 + 4.75*s + 1)
+s = symbols("s")
+h = [1.0, 0.25, 12], [1.0, 4.75, 1]
+hs_zn = []
+for i in h[1]:
+    hs_zn.append(i)
+hs_zn.append(0)
+asd = np.exp(12 + 5.j)
+hs = h[0], hs_zn
+t_z = np.linspace(1,5,num=50)
+res = talbot_inverse(h, t_z)
+fun = 2.0*s/(1.0*s**2 + 3.0*s + 2.0)
 pass
